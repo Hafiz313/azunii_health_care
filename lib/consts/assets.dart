bb04b8f5f40ffad8e00201b99469c44ad38990a7
Muhammad Talha <talha03366987154@gmail.com>
2025-11-15 22:49:49 +0500

    caregiver ui

--- lib/consts/assets.dart
+++ lib/consts/assets.dart
@@ -1,7 +1,6 @@
 class AppAssets {
   const AppAssets._();
 
-
   // //Asset image
   static const String logo = 'assets/logo/logo.png';
   static const String profile = 'assets/images/profileIAvitar.png';
@@ -11,6 +10,32 @@ class AppAssets {
   static const String bell = 'assets/svg/bell.svg';
   static const String attendanceHistory = 'assets/svg/attendanceHistory.svg';
   static const String remender = 'assets/svg/remender.svg';
+  static const String access = 'assets/svg/access.svg';
+  static const String alert = 'assets/svg/alert.svg';
+  static const String caregivers = 'assets/svg/caregivers.svg';
+  static const String categories = 'assets/svg/categories.svg';
+  static const String closeCircle = 'assets/svg/close-circle.svg';
+  static const String date = 'assets/svg/date.svg';
+  static const String dosage = 'assets/svg/dosage.svg';
+  static const String frame4 = 'assets/svg/Frame 4.svg';
+  static const String health = 'assets/svg/health.svg';
+  static const String logoMain = 'assets/svg/logomain.svg';
+  static const String mail = 'assets/svg/mail.svg';
+  static const String medicine = 'assets/svg/medicine.svg';
+  static const String messages = 'assets/svg/messages.svg';
+  static const String note2 = 'assets/svg/note-2.svg';
+  static const String note = 'assets/svg/note.svg';
+  static const String notificationBing = 'assets/svg/notification-bing.svg';
+  static const String notification = 'assets/svg/notification.svg';
+  static const String person = 'assets/svg/person.svg';
+  static const String pills = 'assets/svg/pills.svg';
+  static const String privacy = 'assets/svg/privacy.svg';
+  static const String reviewMed = 'assets/svg/reviewmed.svg';
+  static const String status = 'assets/svg/status.svg';
+  static const String time = 'assets/svg/time.svg';
+  static const String timeline = 'assets/svg/timeline.svg';
+  static const String upload = 'assets/svg/upload.svg';
+  static const String visits = 'assets/svg/visits.svg';
   static const String googleIcon = 'assets/icons/google.svg';
   static const String appleIcon = 'assets/icons/apple.svg';
 }
